bae94e9cb3a39c7f3a217313aadb7efdc58b14d4
efabian1512 <efabian1989@hotmail.com>
2024-11-20 04:01:35 -0400

    feat/adding a not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
 import { ClientListComponent } from './client/client-list/client-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
         { path: '', component: ClientListComponent },
         { path: 'client', component: ClientComponent },
-        { path:'client/edit/:id',component: ClientComponent}
+        { path:'client/edit/:id',component: ClientComponent},
+        { path: 'not-found', component: NotFoundComponent },
+        { path: '**', redirectTo: 'not-found' }
     ];
